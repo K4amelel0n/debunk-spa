da23916cad7d15155a9e37bdc9c1b06014ed4c06
Dawid Kozaczuk <dk312091@student.polsl.pl>
2025-11-27 18:26:37 +0100

    feat: implement authentication layout and login page with form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,34 +1,40 @@
+// src/components/auth/LoginForm.jsx (Lub src/pages/auth/LoginPage.jsx, jeśli chcesz zachować logikę w pages/)
+
 import { Form } from 'react-router';
 
 const LoginForm = () => {
   return (
-    <Form method="post">
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="legend">Logowanie</legend>
+    <Form method="post" className="flex flex-col gap-4">
+      <h3 className="text-2xl font-bold mb-4 text-center">Witaj z powrotem!</h3>
+      <p className="mb-4 text-center text-gray-500">
+        Zaloguj się, aby kontynuować
+      </p>
 
-        <label className="label">Adres email</label>
+      <label className="label">
+        <span className="label-text">Adres email</span>
+      </label>
+      <input
+        type="email"
+        name="email"
+        className="input input-bordered w-full"
+        required
+        placeholder="Wprowadź adres email"
+      />
 
-        <input
-          type="email"
-          name="email"
-          className="input"
-          required
-          placeholder="Wprowadź adres email"
-        />
+      <label className="label">
+        <span className="label-text">Hasło</span>
+      </label>
+      <input
+        type="password"
+        name="password"
+        className="input input-bordered w-full"
+        required
+        placeholder="Wprowadź hasło"
+      />
 
-        <label className="label">Hasło</label>
-        <input
-          type="password"
-          name="password"
-          className="input"
-          required
-          placeholder="Wprowadź hasło"
-        />
-
-        <button type="submit" className="btn btn-neutral mt-4">
-          Zaloguj się
-        </button>
-      </fieldset>
+      <button type="submit" className="btn btn-primary mt-4">
+        Zaloguj się
+      </button>
     </Form>
   );
 };
